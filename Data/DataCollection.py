053a08befbf03c87760af57d30625a932ca55996
Aryan Rishi Lamba <aryanrishi20@gmail.com>
2022-02-18 01:22:56 +0530

    bug fixes

--- Data/DataCollection.py
+++ Data/DataCollection.py
@@ -1,14 +1,13 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
-# options = webdriver.ChromeOptions() ;
-# prefs = {"download.default_directory" : "<directory_path>;
-# prefs = {"download.default_directory" : "D:\Tutorial\down"};
-# options.add_experimental_option("prefs",prefs);
+options = webdriver.ChromeOptions() ;
+prefs = {"download.default_directory" : "D:\\Stocks-Data\\Uber\\"}
+options.add_experimental_option("prefs",prefs)
+driver = webdriver.Chrome(chrome_options=options)
 
-driver = webdriver.Chrome()
+# driver = webdriver.Chrome()
 
 driver.get("https://finance.yahoo.com/quote/UBER/history?p=UBER")
 print(driver.title)
@@ -20,7 +19,6 @@ driver.find_element(By.XPATH, '//*[@id="dropdown-menu"]/div/ul[1]/li[2]/button')
 driver.find_element(By.XPATH, '//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[1]/div[1]/button').click()
 
 driver.find_element(By.XPATH, '//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[1]/div[2]/span[2]/a').click()
-print("hlo")
 
-time.sleep(15)
+time.sleep(60)
 driver.quit()

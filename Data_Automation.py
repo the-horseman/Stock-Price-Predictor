371c70c7a72476452844b55c35416d6c49135158
Aryan Rishi Lamba <aryanrishi20@gmail.com>
2022-03-19 18:05:01 +0550

    Commit on 19/03/2022 17:45:00

--- Data_Automation.py
+++ Data_Automation.py
@@ -39,3 +39,17 @@ TatStl.tatstl_run()
 TvsMtr.tvsmotr_run()
 Upl.upl_run()
 Wpro.wpro_run()
+
+from git import Repo
+from datetime import datetime
+import os
+
+now = datetime.now()
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+Path_of_Repo = os.path.dirname(os.path.realpath(__file__)) + "/.git"
+repo = Repo(Path_of_Repo)
+repo.git.add(update=True)
+repo.index.commit("Commit on " + dt_string)
+origin = repo.remote(name='origin')
+origin.push()

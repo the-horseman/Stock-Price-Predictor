4c124a15b64826f4184e155f5fbfb3c0c8f702a5
Aryan Rishi Lamba <aryanrishi20@gmail.com>
2022-03-19 19:24:38 +0550

    Commited on 19/03/2022 19:04:38

--- Data_Automation.py
+++ Data_Automation.py
@@ -50,6 +50,6 @@ dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 Path_of_Repo = os.path.dirname(os.path.realpath(__file__)) + "/.git"
 repo = Repo(Path_of_Repo)
 repo.git.add(update=True)
-repo.index.commit("Commit on " + dt_string)
+repo.index.commit("Data Automation Commit for " + dt_string)
 origin = repo.remote(name='origin')
 origin.push()

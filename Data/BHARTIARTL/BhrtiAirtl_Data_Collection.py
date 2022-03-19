bb05e6e601c37009e6b44ae5be35afdeeda7db5b
Aryan Rishi Lamba <aryanrishi20@gmail.com>
2022-03-19 12:08:12 +0530

    Added Data Automation

---
+++ Data/BHARTIARTL/BhrtiAirtl_Data_Collection.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import os
+import pandas as pd
+
+def BhrtiAirtl_Data():
+    options = webdriver.ChromeOptions() ;
+    prefs = {"download.default_directory" : os.path.dirname(os.path.realpath(__file__))}
+    options.add_experimental_option("prefs",prefs)
+    driver = webdriver.Chrome(chrome_options=options)
+
+    # driver = webdriver.Chrome()
+
+    driver.get("https://finance.yahoo.com/quote/BHARTIARTL.NS/history?p=BHARTIARTL.NS")
+    print(driver.title)
+
+    driver.find_element(By.XPATH, '//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[1]/div[1]/div[1]/div/div').click()
+
+    driver.find_element(By.XPATH, '//*[@id="dropdown-menu"]/div/ul[1]/li[1]/button').click()
+
+    driver.find_element(By.XPATH, '//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[1]/div[1]/button').click()
+
+    driver.find_element(By.XPATH, '//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[1]/div[2]/span[2]/a').click()
+
+    time.sleep(30)
+    driver.quit()
+
+def bhrtArtl_data_add():
+    path1 = "BHARTIARTL.NS.csv"
+    path2 = "BHARTIARTL.NS (1).csv"
+    dataframe1 = pd.read_csv(path1)
+    findat1 = dataframe1.tail(1).to_numpy().tolist()
+    dataframe2 = pd.read_csv(path2)
+    findat2 = dataframe2.tail(1).to_numpy().tolist()
+    print(findat1 == findat2)
+    if (findat1 != findat2):
+        with open(path1, 'a', newline='') as f_object:
+            f_object.write(",".join(list(map(str,findat2[0])))+"\n")
+
+    os.remove(path2)
+
+def bhrtArtl_run():
+    BhrtiAirtl_Data()
+    bhrtArtl_run()
---
+++ Data_Automation.py
@@ -0,0 +1,41 @@
+import Data.AXISBANK.AxisBank_Data_Collection as AxsBnk
+import Data.BALKRISIND.Balkrishna_Data_Collection as BalKrish
+import Data.BANKBARODA.BnkOfBaroda_Data_Collection as BnkBrda
+import Data.BEL.Bel_Data_Collection as Bel
+import Data.BHARTIARTL.BhrtiAirtl_Data_Collection as BhrtArtl
+import Data.CANBK.CanraBnk_Data_Collection as CanBnk
+import Data.EASEMYTRIP.EaseMyTrip_Data_Collection as EseMyTrp
+import Data.GHCL.GHCL_Data_Collection as Ghcl
+import Data.HDFCBANK.HDFC_Data_Collection as Hdfc
+import Data.ICICIBANK.ICICI_Data_Collection as Icici
+import Data.JUBLFOOD.Jubl_Food_Data_Collection as JblFood
+import Data.KPITTECH.KPIT_Data_Collection as Kpit
+import Data.LT.LNT_Data_Collection as Lnt
+import Data.PRAJIND.PRAJ_Data_Collection as Praj
+import Data.RELIANCE.Reliance_Data_Collection as Rel
+import Data.TATAMOTORS.TataMot_Data_Collection as TatMotr
+import Data.TATASTEEL.Tatasteel_Data_Collection as TatStl
+import Data.TVSMOTOR.TVS_Data_Collection as TvsMtr
+import Data.UPL.UPL_Data_Collection as Upl
+import Data.WIPRO.Wipro_Data_Collection as Wpro
+
+AxsBnk.axis_run()
+BalKrish.balkrish_run()
+BnkBrda.broda_run()
+Bel.bel_run()
+BhrtArtl.bhrtArtl_run()
+CanBnk.cnra_run()
+EseMyTrp.esemytrp_run()
+Ghcl.ghcl_run()
+Hdfc.hdfc_run()
+Icici.icici_run()
+JblFood.jblfood_run()
+Kpit.kpit_run()
+Lnt.lnt_run()
+Praj.praj_run()
+Rel.Rel_run()
+TatMotr.tatmotr_run()
+TatStl.tatstl_run()
+TvsMtr.tvsmotr_run()
+Upl.upl_run()
+Wpro.wpro_run()

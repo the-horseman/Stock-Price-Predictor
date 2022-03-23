9c416bf895389bb771104b4adaec1d32f311afbb
Aryan Rishi Lamba <aryanrishi20@gmail.com>
2022-03-23 23:46:51 +0530

    Added AxisBank Prediction Model

--- Data/AXISBANK/AxisBank_Data_Collection.py
+++ Data/AXISBANK/AxisBank_Data_Collection.py
@@ -44,8 +44,24 @@ def AxisBnk_data_add():
     if (findat1 != findat2):
         with open(path1, 'a', newline='') as f_object:
             f_object.writelines(",".join(list(map(str,findat2[0])))+"\n")
-
+        AxisBnk_train()
     os.remove(path2)
+
+def AxisBnk_train():
+    import joblib
+    import tensorflow as tf
+    from keras.preprocessing.sequence import TimeseriesGenerator
+    sc_path = os.path.dirname(os.path.realpath(__file__)) + "/AXISBANK_scaler.pkl"
+    md_path = os.path.dirname(os.path.realpath(__file__)) + "/AXISBANK_model.h5"
+    fil_path = os.path.dirname(os.path.realpath(__file__)) + "/AXISBANK.NS.csv"
+    scaler = joblib.load(sc_path)
+    rnn = tf.keras.models.load_model(md_path)
+    df = pd.read_csv(fil_path, usecols=["Date", "Open", "Adj Close", "High", "Low"], index_col='Date',parse_dates=True)
+    df = df[-2::]
+    sc_t = scaler.transform(df)
+    gene = TimeseriesGenerator(sc_t, sc_t, 1, batch_size=1)
+    rnn.fit(gene, epochs=10)
+    rnn.save("AXISBANK_model.h5")
 
 def axis_run():
     AxisBnk_Data()
